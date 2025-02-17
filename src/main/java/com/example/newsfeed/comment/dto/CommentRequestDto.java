3389bedeac4284ae70f21c351b811b2fab9c0a6f
이희수 <leehsdd33@gmail.com>
2025-02-17 14:18:52 +0900

    feat: 댓글 등록 API 구현

--- src/main/java/com/example/newsfeed/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/newsfeed/comment/dto/CommentRequestDto.java
@@ -1,4 +1,9 @@
 package com.example.newsfeed.comment.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentRequestDto {
+    private Long userId;
+    private String content;
 }
--- src/main/java/com/example/newsfeed/comment/dto/CommentResponseDto.java
+++ src/main/java/com/example/newsfeed/comment/dto/CommentResponseDto.java
@@ -1,4 +1,18 @@
 package com.example.newsfeed.comment.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentResponseDto {
+    private final Long id;
+    private final Long userId;
+    private final Long postId;
+    private final String content;
+
+    public CommentResponseDto(Long id, Long userId, Long postId, String content) {
+        this.id = id;
+        this.userId = userId;
+        this.postId = postId;
+        this.content = content;
+    }
 }
