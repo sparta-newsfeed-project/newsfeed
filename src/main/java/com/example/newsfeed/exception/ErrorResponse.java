c148b9c2ff601b28a81f7264e53192fc5c6a2e8e
subbni <slfkak@naver.com>
2025-02-17 10:34:03 +0900

    feat: exception 처리 모듈 생성 (#1)

---
+++ src/main/java/com/example/newsfeed/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.example.newsfeed.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private ExceptionType type;
+    private String message;
+
+    public ErrorResponse(ExceptionType type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public static ErrorResponse from(CustomException e) {
+        return new ErrorResponse(e.getExceptionType(), e.getMessage());
+    }
+}
---
+++ src/main/java/com/example/newsfeed/exception/ExceptionType.java
@@ -0,0 +1,29 @@
+package com.example.newsfeed.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExceptionType {
+
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청값 검증에 실패했습니다."),
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "해당 이메일로 가입한 계정이 존재합니다."),
+
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "로그인 정보가 올바르지 않습니다."),
+
+    NO_PERMISSION_ACTION(HttpStatus.FORBIDDEN, "권한이 없는 작업입니다."),
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    ExceptionType(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
