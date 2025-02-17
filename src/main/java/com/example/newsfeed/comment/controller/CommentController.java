dbb383e89a0c2d65665fb0d51d6e427bb618adf2
이희수 <leehsdd33@gmail.com>
2025-02-17 21:38:43 +0900

    feat: 댓글 다건 조회 페이지네이션 구현

--- src/main/java/com/example/newsfeed/comment/controller/CommentController.java
+++ src/main/java/com/example/newsfeed/comment/controller/CommentController.java
@@ -1,11 +1,7 @@
 package com.example.newsfeed.comment.controller;
 
-<<<<<<< HEAD
 import com.example.newsfeed.comment.dto.CommentCreateRequestDto;
-=======
 import com.example.newsfeed.comment.dto.CommentDetailResponseDto;
-import com.example.newsfeed.comment.dto.CommentRequestDto;
->>>>>>> 619b809b6960ce7f164c252fbf7481a8039a99db
 import com.example.newsfeed.comment.dto.CommentSimpleResponseDto;
 import com.example.newsfeed.comment.pagination.Paging;
 import com.example.newsfeed.comment.service.CommentService;
@@ -15,11 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-<<<<<<< HEAD
 import java.time.LocalDate;
-=======
 import java.util.List;
->>>>>>> 619b809b6960ce7f164c252fbf7481a8039a99db
 
 @RestController
 @RequiredArgsConstructor
@@ -31,7 +24,6 @@ public class CommentController {
         return ResponseEntity.ok(commentService.saveComment(postId, dto));
     }
 
-<<<<<<< HEAD
     @GetMapping
     public ResponseEntity<Paging.Response> getAllComments(
             @RequestParam(required = false) Long postId,
@@ -40,10 +32,6 @@ public class CommentController {
             @RequestParam(required = false, defaultValue = "0") int page
     ) {
         return new ResponseEntity<>(commentService.getAllComments(postId, updatedAt, new Paging.Request(size, page)), HttpStatus.OK);
-=======
-    @GetMapping("/api/posts/{postId}/comments")
-    public ResponseEntity<List<CommentDetailResponseDto>> getAllComments(@PathVariable Long postId) {
-        return ResponseEntity.ok(commentService.getAllComments(postId));
->>>>>>> 619b809b6960ce7f164c252fbf7481a8039a99db
     }
+
 }
--- src/main/java/com/example/newsfeed/comment/service/CommentService.java
+++ src/main/java/com/example/newsfeed/comment/service/CommentService.java
@@ -2,7 +2,9 @@ package com.example.newsfeed.comment.service;
 
 import com.example.newsfeed.comment.domain.Comment;
 import com.example.newsfeed.comment.dto.CommentCreateRequestDto;
+import com.example.newsfeed.comment.dto.CommentResponseDto;
 import com.example.newsfeed.comment.dto.CommentSimpleResponseDto;
+import com.example.newsfeed.comment.pagination.Paging;
 import com.example.newsfeed.comment.repository.CommentRepository;
 import com.example.newsfeed.post.domain.Post;
 import com.example.newsfeed.post.repository.PostRepository;
@@ -10,6 +12,9 @@ import com.example.newsfeed.user.domain.User;
 import com.example.newsfeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +41,18 @@ public class CommentService {
                 savedComment.getPost().getId(),
                 savedComment.getContent()
         );
+    }
+
+    public Paging.Response getAllComments(Long postId, LocalDate updatedAt, Paging.Request pagingRequest) {
+        List<CommentResponseDto> pagingData =  commentRepository.findAllByPostIdAndUpdatedAt(postId, updatedAt, pagingRequest)
+                .stream()
+                .map(CommentResponseDto::from)
+                .toList();
+
+        return Paging.Response.builder()
+                .data(pagingData.isEmpty() ? new Object[0] : pagingData.toArray())
+                .size(pagingRequest.getSize())
+                .page(pagingRequest.getPage())
+                .build();
     }
 }
