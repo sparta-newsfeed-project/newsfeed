dcd58e57eafd5c30bfb8a536ef3ed4e408e66048
subbni <slfkak@naver.com>
2025-02-17 10:34:32 +0900

    docs: sql 파일 작성 (#1)

---
+++ newsfeed.sql
@@ -0,0 +1,46 @@
+CREATE TABLE users (
+    id  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email   VARCHAR(255) NOT NULL UNIQUE,
+    name    VARCHAR(100) NOT NULL,
+    intro_text VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    created_at  DATETIME NOT NULL,
+    updated_at  DATETIME NOT NULL,
+    deleted_at DATETIME DEFAULT NULL
+);
+
+CREATE TABLE posts (
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id     BIGINT NOT NULL,
+    title    VARCHAR(100) NOT NULL,
+    content   TEXT NOT NULL,
+    created_at  DATETIME NOT NULL,
+    updated_at  DATETIME NOT NULL,
+    deleted_at DATETIME DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id     BIGINT NOT NULL,
+    post_id     BIGINT NOT NULL,
+    content   VARCHAR(255) NOT NULL,
+    created_at  DATETIME NOT NULL,
+    updated_at  DATETIME NOT NULL,
+    deleted_at DATETIME DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+);
+
+CREATE TABLE follows (
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    follower_id     BIGINT NOT NULL,
+    following_id     BIGINT NOT NULL,
+    created_at  DATETIME NOT NULL,
+    updated_at  DATETIME NOT NULL,
+    deleted_at DATETIME DEFAULT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (following_id) REFERENCES users(id)
+);
+
+
