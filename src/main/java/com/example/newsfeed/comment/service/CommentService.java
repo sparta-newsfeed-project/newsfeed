b9eabac0dcc09d16114e26f56e37f0bae9caa269
이희수 <leehsdd33@gmail.com>
2025-02-18 23:47:27 +0900

    refactor: 댓글 조회 페이지네이션 공통응답객체 적용 (#37)

--- src/main/java/com/example/newsfeed/comment/service/CommentService.java
+++ src/main/java/com/example/newsfeed/comment/service/CommentService.java
@@ -4,16 +4,19 @@ import com.example.newsfeed.comment.domain.Comment;
 import com.example.newsfeed.comment.dto.CommentCreateRequestDto;
 import com.example.newsfeed.comment.dto.CommentResponseDto;
 import com.example.newsfeed.comment.dto.CommentSimpleResponseDto;
-import com.example.newsfeed.comment.pagination.Paging;
 import com.example.newsfeed.comment.repository.CommentRepository;
+import com.example.newsfeed.global.pagination.PaginationResponse;
 import com.example.newsfeed.post.domain.Post;
 import com.example.newsfeed.post.repository.PostRepository;
 import com.example.newsfeed.user.domain.User;
 import com.example.newsfeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -42,13 +45,12 @@ public class CommentService {
         );
     }
 
-    public Page<CommentResponseDto> getAllComments(Long postId, Paging.Request pagingRequest) {
-        // TODO : 1. updatedAt 내림차순으로 정렬되도록 만들어주세요
-        // TODO : 2. Pagination 공통 객체가 생성되면 해당 객체를 반환하도록 수정해주세요
+    // TODO : 1. updatedAt 내림차순으로 정렬되도록 만들어주세요
+    // TODO : 2. Pagination 공통 객체가 생성되면 해당 객체를 반환하도록 수정해주세요
+    @Transactional(readOnly = true)
+    public PaginationResponse<CommentResponseDto> getAllComments(Long postId, Pageable pageable) {
+        Page<Comment> comments =  commentRepository.findAllByPostId(postId, pageable);
 
-        PageRequest pageRequest = PageRequest.of(pagingRequest.getPage(), pagingRequest.getSize());
-        Page<Comment> comments =  commentRepository.findAllByPostId(postId,pageRequest);
-
-        return comments.map(comment -> CommentResponseDto.from(comment));
+        return new PaginationResponse<>(comments.map(CommentResponseDto::from));
     }
 }
