6317e72c9114b6ed29d13de1e466e91f70aac31f
subbni <slfkak@naver.com>
2025-02-18 17:26:50 +0900

    fix: 댓글 목록 페이지네이션 조회 에러 수정 (#32) - TODO 주석 처리 된 부분 추후 구현 바랍니다.

--- src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -1,13 +1,10 @@
 package com.example.newsfeed.comment.repository;
 
 import com.example.newsfeed.comment.domain.Comment;
-import com.example.newsfeed.comment.dto.CommentDetailResponseDto;
-import com.example.newsfeed.comment.pagination.Paging;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<CommentDetailResponseDto> findAllByPostIdAndUpdatedAt(Long postId, LocalDate updatedAt, Paging.Request pagingRequest);
+    Page<Comment> findAllByPostId(Long postId, Pageable pageable);
 }
