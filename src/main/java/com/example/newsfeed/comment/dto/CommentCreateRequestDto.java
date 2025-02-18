e87d815be8bc072015864b6d9f94aa8219d2e8c4
이희수 <leehsdd33@gmail.com>
2025-02-18 10:32:24 +0900

    Revert "댓글 조회 API 개발 "

--- src/main/java/com/example/newsfeed/comment/dto/CommentCreateRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.newsfeed.comment.dto;
-
-import lombok.Getter;
-
-@Getter
-public class CommentCreateRequestDto {
-    private Long userId;
-    private String content;
-}
--- src/main/java/com/example/newsfeed/user/repository/UserRepository.java
+++ src/main/java/com/example/newsfeed/user/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.newsfeed.user.repository;
 
-import com.example.newsfeed.comment.domain.Comment;
 import com.example.newsfeed.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByComment(Comment comment);
 }
