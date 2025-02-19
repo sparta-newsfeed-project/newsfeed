39df59135c31a4461744a663e9d450e6dbaed087
이희수 <leehsdd33@gmail.com>
2025-02-19 15:54:32 +0900

    feat: 댓글 삭제 시 댓글수 동기화 기능 구현 (#57)

--- src/main/java/com/example/newsfeed/comment/service/CommentService.java
+++ src/main/java/com/example/newsfeed/comment/service/CommentService.java
@@ -91,9 +91,16 @@ public class CommentService {
                 () -> new CustomException(ExceptionType.COMMENT_NOT_FOUND)
         );
 
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new CustomException(ExceptionType.POST_NOT_FOUND)
+        );
+
         isAuthorizedCommentEditor(userId, comment);
 
         checkPostIdOfComment(postId, comment);
+
+        post.decreaseCommentCount();
+        postRepository.save(post);
 
         commentRepository.deleteById(commentId);
     }
