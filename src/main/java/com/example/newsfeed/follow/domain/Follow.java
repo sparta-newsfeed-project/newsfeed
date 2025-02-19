f663050fd6f9651ea323eb84861ada3c2db6e51e
subbni <slfkak@naver.com>
2025-02-19 14:15:26 +0900

    feat: Follow 조회 시 soft delete 제외 적용 (#53)

--- src/main/java/com/example/newsfeed/follow/domain/Follow.java
+++ src/main/java/com/example/newsfeed/follow/domain/Follow.java
@@ -7,11 +7,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "follows")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
 public class Follow extends BaseTimeEntity {
 
     @Id
