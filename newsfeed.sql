08611dbc21ea79366c3ad809e18fab6d6a7c2622
subbni <slfkak@naver.com>
2025-02-19 13:26:43 +0900

    docs: sql - posts에 comment_count 컬럼 추가 (#45) - 프로젝트 성격 상 Post 조회가 핵심 기능 - Post 조회 시 매번 Join 연산을 하는 것보다 컬럼을 하나 추가하는 것이 좋겠다는 판단하에 컬럼을 추가합니다. - posts 테이블에  컬럼을 두고, 댓글을 추가/삭제할 때마다 +1, -1 수행

--- newsfeed.sql
+++ newsfeed.sql
@@ -14,6 +14,7 @@ CREATE TABLE posts (
     user_id     BIGINT NOT NULL,
     title    VARCHAR(100) NOT NULL,
     content   TEXT NOT NULL,
+    comment_count INT UNSIGNED NOT NULL DEFAULT 0,
     created_at  DATETIME NOT NULL,
     updated_at  DATETIME NOT NULL,
     deleted_at DATETIME DEFAULT NULL,
